0bdac4a5e6cfaec7c6617cb815813d25582bd241
Wilson Ruan <wilsonruan23@gmail.com>
2020-10-09 04:53:41 -0400

    Add more to FILES TO CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,9 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
+    "styles.css",
+    "index.js",
+    "/manifest.webmanifest"
 ]
 
 const CACHE_NAME = "static-cache-v2";
