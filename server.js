18d5164020e3d47d325e59c7446b899f53caba98
Wilson Ruan <wilsonruan23@gmail.com>
2020-10-09 05:43:38 -0400

    deploying

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+mongoose.connect(
+  MONGODB_URI, 
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
